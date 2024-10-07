7928a345caeb15eef3d9cdb74c783124a0e5f757
Sapin5 <123406040+Sapin5@users.noreply.github.com>
2024-10-07 14:23:17 -0500

    Modified Player mechanics
    Created prototype dash ability
    Created basic movement mechanics (Jumping, walking)
    Created prototype attack mechanics

--- Assets/Scripts/Moveme.cs
+++ Assets/Scripts/Moveme.cs
@@ -1,26 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Windows.Input;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Moveme : MonoBehaviour
 {
-    [SerializeField] private float fallSpeed;
+    [SerializeField]private bool touchingGround;
+    [SerializeField] private float jumpForce = 10f;
     private Rigidbody2D physicsBody;
-    private static bool touchingGround;
+
+    public enum Dash {Ready, Dashing,End, CoolDown};
+    public Dash dash;
+    private Transform player;
+
+    private float timer;
     private void Start()
     {
+        player = GetComponent<Transform>().GetChild(0);
         physicsBody = GetComponent<Rigidbody2D>();
     }
     void Update()
-    {
-        
-        transform.Translate(Input.GetAxis("Horizontal") *
-                                    Time.deltaTime * 10, 0, 0 );
+    {   
+        Jumping();
+        Attack();
+        Movement();
+        DashAbility();
+    }
 
+    private void DashAbility(){
+        switch(dash){
+            case Dash.Ready:
+                if(Input.GetKeyDown(KeyCode.LeftShift)){
+                //transform.position = new Vector3(transform.position.x+2f, transform.position.y, 0);
+                //physicsBody.AddForce(Vector2.right*4, ForceMode2D.Impulse);
+                dash = Dash.Dashing;
+                }
+                break;
+            case Dash.Dashing:
+                physicsBody.constraints = RigidbodyConstraints2D.FreezePositionY;
+                physicsBody.AddForce(Vector2.right*4, ForceMode2D.Impulse);
+                Timer(0.1f, Dash.End);
+                break;
+            case Dash.End:
+                physicsBody.constraints = RigidbodyConstraints2D.FreezeAll;
+                physicsBody.constraints = RigidbodyConstraints2D.None;
+                physicsBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+                dash = Dash.CoolDown;
+                break;
+            case Dash.CoolDown:
+                Timer(1f, Dash.Ready);
+                break;
+        }
+    }
+    public void Timer(float delay, Dash dash){
+        timer+=Time.deltaTime;
+        Debug.Log($"Cooldown for: {dash}, {delay}");
+        // Switches when timer becomes greater than the delay
+        if(timer>delay){
+            // resets timer
+            timer=0;
+            // Switches firingState to whatver state was passed
+            this.dash = dash;
+        }
+    }
+
+    private void Movement(){
+        float dir = Input.GetAxis("Horizontal");
+
+        if (dir != 0) {
+            transform.rotation = Quaternion.Euler(0, dir > 0 ? 0 : 180, 0);
+            transform.Translate((dir > 0 ? dir : -dir) * Time.deltaTime * 10, 0, 0);
+        }
+    }
+
+    private void Attack(){
+        if(Input.GetKey(KeyCode.Space)){
+            player.GameObject().SetActive(true);
+        }else{
+             player.GameObject().SetActive(false);
+        }
+    }
+    
+    private void Jumping(){
         if((Input.GetKeyDown(KeyCode.UpArrow)|| 
                 Input.GetKeyDown(KeyCode.W))&& touchingGround){
-            physicsBody.AddForce(Vector2.up * fallSpeed, ForceMode2D.Impulse);
+            physicsBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             touchingGround = false;
         }
 
@@ -29,15 +94,9 @@ public class Moveme : MonoBehaviour
             physicsBody.AddForce(Vector2.down/2, ForceMode2D.Impulse);
             Debug.Log("Going Down");
         }
-        Debug.Log(touchingGround);
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.name == "Square"){
-            touchingGround = true;
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D collision){
         if(collision.gameObject.name == "Square"){
             touchingGround = true;
         }
