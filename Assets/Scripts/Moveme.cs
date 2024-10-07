2fd6ddd3c75eebb4d99f9126cde631d0a8b6c28a
Sapin5 <123406040+Sapin5@users.noreply.github.com>
2024-10-07 15:32:47 -0500

    Fixed Dash mechanics so it travels a consistant distance
    Added some sprotes to visualize direction easier

--- Assets/Scripts/Moveme.cs
+++ Assets/Scripts/Moveme.cs
@@ -1,61 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Animations;
 
 public class Moveme : MonoBehaviour
 {
-    [SerializeField]private bool touchingGround;
-    [SerializeField] private float jumpForce = 10f;
+    [SerializeField]private float jumpForce, dashForce;
     private Rigidbody2D physicsBody;
 
     public enum Dash {Ready, Dashing,End, CoolDown};
     public Dash dash;
     private Transform player;
-
-    private float timer;
+    private bool touchingGround;
+    private float timer, target;
     private void Start()
     {
         player = GetComponent<Transform>().GetChild(0);
         physicsBody = GetComponent<Rigidbody2D>();
     }
     void Update()
-    {   
-        Jumping();
+    {
         Attack();
         Movement();
+        Jumping();
+    }
+
+    void FixedUpdate(){
         DashAbility();
     }
 
     private void DashAbility(){
+        int direction = 1;
+        if(transform.rotation.y == -1){
+            Debug.Log("Facing left");
+            direction = 1;
+        }else if (transform.rotation.y == 0){
+            Debug.Log("Facing Right "  + transform.rotation.y);
+            direction = -1;
+        }
+
         switch(dash){
             case Dash.Ready:
                 if(Input.GetKeyDown(KeyCode.LeftShift)){
                 //transform.position = new Vector3(transform.position.x+2f, transform.position.y, 0);
                 //physicsBody.AddForce(Vector2.right*4, ForceMode2D.Impulse);
-                dash = Dash.Dashing;
+                    dash = Dash.Dashing;
+                    target = Time.time+0.1f;
                 }
                 break;
             case Dash.Dashing:
+                
                 physicsBody.constraints = RigidbodyConstraints2D.FreezePositionY;
-                physicsBody.AddForce(Vector2.right*4, ForceMode2D.Impulse);
-                Timer(0.1f, Dash.End);
+                physicsBody.AddForce(dashForce * direction * Vector2.left, ForceMode2D.Impulse);
+                Timer(0.1f, Dash.End, target);
                 break;
             case Dash.End:
                 physicsBody.constraints = RigidbodyConstraints2D.FreezeAll;
                 physicsBody.constraints = RigidbodyConstraints2D.None;
                 physicsBody.constraints = RigidbodyConstraints2D.FreezeRotation;
                 dash = Dash.CoolDown;
+                target = Time.time+1f;
                 break;
             case Dash.CoolDown:
-                Timer(1f, Dash.Ready);
+                Timer(1f, Dash.Ready, target);
                 break;
         }
     }
-    public void Timer(float delay, Dash dash){
+    public void Timer(float delay, Dash dash, float target){
         timer+=Time.deltaTime;
-        Debug.Log($"Cooldown for: {dash}, {delay}");
+        Debug.Log($"{timer} time passed. Target {delay}");
         // Switches when timer becomes greater than the delay
         if(timer>delay){
             // resets timer
@@ -78,7 +93,7 @@ public class Moveme : MonoBehaviour
         if(Input.GetKey(KeyCode.Space)){
             player.GameObject().SetActive(true);
         }else{
-             player.GameObject().SetActive(false);
+            player.GameObject().SetActive(false);
         }
     }
     
