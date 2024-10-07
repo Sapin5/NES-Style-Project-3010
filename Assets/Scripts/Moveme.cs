c5acb38a1e2fcd0f74850d654f8c03780e122600
Sapin5 <123406040+Sapin5@users.noreply.github.com>
2024-10-07 16:11:47 -0500

    Fixed jump bugs

--- Assets/Scripts/Moveme.cs
+++ Assets/Scripts/Moveme.cs
@@ -1,20 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.Animations;
 
 public class Moveme : MonoBehaviour
 {
     [SerializeField]private float jumpForce, dashForce;
     private Rigidbody2D physicsBody;
-
     public enum Dash {Ready, Dashing,End, CoolDown};
     public Dash dash;
     private Transform player;
     private bool touchingGround;
-    private float timer, target;
+    private float timer;
     private void Start()
     {
         player = GetComponent<Transform>().GetChild(0);
@@ -23,54 +18,50 @@ public class Moveme : MonoBehaviour
     void Update()
     {
         Attack();
-        Movement();
         Jumping();
     }
 
     void FixedUpdate(){
+        Movement();
         DashAbility();
     }
 
     private void DashAbility(){
         int direction = 1;
         if(transform.rotation.y == -1){
-            Debug.Log("Facing left");
             direction = 1;
         }else if (transform.rotation.y == 0){
-            Debug.Log("Facing Right "  + transform.rotation.y);
             direction = -1;
         }
 
         switch(dash){
             case Dash.Ready:
-                if(Input.GetKeyDown(KeyCode.LeftShift)){
-                //transform.position = new Vector3(transform.position.x+2f, transform.position.y, 0);
-                //physicsBody.AddForce(Vector2.right*4, ForceMode2D.Impulse);
+                if(Input.GetKey(KeyCode.LeftShift)){
                     dash = Dash.Dashing;
-                    target = Time.time+0.1f;
                 }
+                Debug.Log("Ready to Dash");
                 break;
             case Dash.Dashing:
-                
                 physicsBody.constraints = RigidbodyConstraints2D.FreezePositionY;
                 physicsBody.AddForce(dashForce * direction * Vector2.left, ForceMode2D.Impulse);
-                Timer(0.1f, Dash.End, target);
+                Timer(0.1f, Dash.End);
+                Debug.Log("Dashing");
                 break;
             case Dash.End:
                 physicsBody.constraints = RigidbodyConstraints2D.FreezeAll;
                 physicsBody.constraints = RigidbodyConstraints2D.None;
                 physicsBody.constraints = RigidbodyConstraints2D.FreezeRotation;
                 dash = Dash.CoolDown;
-                target = Time.time+1f;
+                Debug.Log("Dash Ended");
                 break;
             case Dash.CoolDown:
-                Timer(1f, Dash.Ready, target);
+                Debug.Log("On Cooldown");
+                Timer(1f, Dash.Ready);
                 break;
         }
     }
-    public void Timer(float delay, Dash dash, float target){
+    public void Timer(float delay, Dash dash){
         timer+=Time.deltaTime;
-        Debug.Log($"{timer} time passed. Target {delay}");
         // Switches when timer becomes greater than the delay
         if(timer>delay){
             // resets timer
@@ -83,7 +74,7 @@ public class Moveme : MonoBehaviour
     private void Movement(){
         float dir = Input.GetAxis("Horizontal");
 
-        if (dir != 0) {
+        if (dir != 0) { 
             transform.rotation = Quaternion.Euler(0, dir > 0 ? 0 : 180, 0);
             transform.Translate((dir > 0 ? dir : -dir) * Time.deltaTime * 10, 0, 0);
         }
@@ -114,6 +105,7 @@ public class Moveme : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.name == "Square"){
             touchingGround = true;
+            Debug.Log("Hit ground");
         }
     }
 }
