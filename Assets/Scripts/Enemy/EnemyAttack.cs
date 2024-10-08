12d4d4f96a34e2b889c3c534cf7b24ed99e50b0d
Ken J. <kjimeno2@academic.rrc.ca>
2024-10-08 13:10:13 -0500

    Fixed Enemy Face Direction
    Modified EnemyAttack so that it checks while in the attacking state which side of the enemy the player is at.

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -7,13 +7,19 @@ public class EnemyAttack : MonoBehaviour
     [SerializeField] private GameObject leftWeapon;
     [SerializeField] [Range(0.1f, 10f)] private float attackDuration;
     [SerializeField] [Range(0.5f, 15f)]private float attackSpeed;
+    [SerializeField] private SpriteRenderer enemyRenderer; 
+
     
 
     private float currentTimer;
     private const float MAX_TIME = 5f;
+    private int xDirection = 1;
 
     private void Update() {
         currentTimer += Time.deltaTime * attackSpeed;
+
+        xDirection = (playerPos.position.x - transform.position.x) > 0 ? 1 : -1;
+        enemyRenderer.flipX = xDirection == 1 ? false : true;
 
         if (currentTimer >= MAX_TIME) {
             if (currentTimer >= MAX_TIME + attackDuration)
@@ -28,7 +34,6 @@ public class EnemyAttack : MonoBehaviour
     }
 
     private void DecideWeapon() {
-        int xDirection = (playerPos.position.x - transform.position.x) > 0 ? 1 : -1;
         if (xDirection == 1) {
             rightWeapon.SetActive(true);
             leftWeapon.SetActive(false);
