fd7c46347806312595862f13ea9fd1d14800636a
Ken J. <kjimeno2@academic.rrc.ca>
2024-10-08 15:54:59 -0500

    Implemented a rough knockback feature

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -2,9 +2,19 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
+    [SerializeField] Transform wielderTransform;
     [SerializeField] private float damage;
+    [SerializeField] private float knockbackStrength;
 
     public float GetDamage() {
         return damage;
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.GetComponent<Rigidbody2D>() != null) {
+            Vector2 force = other.transform.position - wielderTransform.position;
+
+            Debug.Log("BOOM");
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(force * knockbackStrength, ForceMode2D.Impulse);
+        }
     }
 }
