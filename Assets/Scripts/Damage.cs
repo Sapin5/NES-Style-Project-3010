7e5620e03b4ecf6fbdbf7d1b66d00c0310b031fa
Ken J. <kjimeno2@academic.rrc.ca>
2024-10-09 13:12:11 -0500

    Knockback fix
    Fixed the knockback effect by making the direction normalized. Also added a debounce timer for the knockback.

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -5,15 +5,24 @@ public class Damage : MonoBehaviour
     [SerializeField] Transform wielderTransform;
     [SerializeField] private float damage;
     [SerializeField] private float knockbackStrength;
+    [SerializeField] [Range(0.1f, 2f)] private float knockbackDebounce;
+
+    private float currentTimer = 0;
 
     public float GetDamage() {
         return damage;
     }
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.GetComponent<Rigidbody2D>() != null) {
-            Vector2 force = other.transform.position - wielderTransform.position;
 
-            other.gameObject.GetComponent<Rigidbody2D>().AddForce(force * knockbackStrength, ForceMode2D.Impulse);
+    private void Update() {
+        currentTimer += Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.GetComponent<Rigidbody2D>() != null && currentTimer >= knockbackDebounce) {
+            Vector2 forceDirection = new Vector2((other.transform.position.x - wielderTransform.position.x) > 0 ? 1 : -1, 0);
+
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(forceDirection * knockbackStrength, ForceMode2D.Impulse);
+
+            currentTimer = 0;
         }
     }
 }
