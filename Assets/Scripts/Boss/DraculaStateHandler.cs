b76406a725ace0a057bbeacb207f1b65b7d54bb2
Ken Jimeno <kbjimeno@gmail.com>
2024-10-19 22:35:44 -0500

    Created the DraculaStateHandler Class
    Also contains short pseudocode for explaining state handling

---
+++ Assets/Scripts/Boss/DraculaStateHandler.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DraculaStateHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Pick and play a random state  
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Pick a random state
+        
+        //if [ATTACK], enable attack animator and turn other states off
+
+        //after attack, move back to original flying spot.
+
+        //once in original spot, idle for ____ amount of seconds.
+
+        // THEN, PICK A RANDOM ATTACK!! 
+
+        //repeat
+
+        //(also if Dracula is attacked, go back to original spot like as explained above, EXCEPT that Dracula is invulnerable)
+    }
+}
