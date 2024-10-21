e52b4b0148a586fc93e7728ee964fdbe430318b6
Ken Jimeno <kbjimeno@gmail.com>
2024-10-20 19:32:08 -0500

    Worked on choosing a random state method

--- Assets/Scripts/Boss/DraculaStateHandler.cs
+++ Assets/Scripts/Boss/DraculaStateHandler.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class DraculaStateHandler : MonoBehaviour
 {
+    [SerializeField] private float maxIdleTime;
+    [SerializeField] private Animator draculaAnimator;
     
     private enum State { Idle, MeleeAttack1 , MeleeAttack2 , ProjectileAttack } ;
     private State currentState;
 
+    private float currentTimer = 0f;
 
     // Start is called before the first frame update
     private void Start()
@@ -18,32 +21,68 @@ public class DraculaStateHandler : MonoBehaviour
     private void Update()
     {
         //Pick a random state
-        
+
         //if [ATTACK], enable attack animator and turn other states off
-
         //after attack, move back to original flying spot.
-
         //once in original spot, idle for ____ amount of seconds.
 
         // THEN, PICK A RANDOM ATTACK!! 
-
         //repeat
-
         //(also if Dracula is attacked, go back to original spot like as explained above, EXCEPT that Dracula is invulnerable)
-    }
+        //-----------------------------------------------------------
+        switch (currentState) {
+            case State.Idle:
+                draculaAnimator.SetBool("Idle", true);
 
-    private State ChooseRandomAttack() {
-        const int numAttacks = 3;
+                if (currentTimer >= maxIdleTime) {
+                    currentState = ChooseRandomState();
+                }
 
-        int randomAttack = Random.Range(1, numAttacks + 1);
-
-        switch (randomAttack) {
-            case 1:
-                return State.MeleeAttack1;
-            case 2:
-                return State.MeleeAttack2;
-            default:
-                return State.ProjectileAttack;
+                return;
+            case State.MeleeAttack1:
+                draculaAnimator.SetBool("MeleeAttack1", true);
+                return;
+            case State.MeleeAttack2:
+                draculaAnimator.SetBool("MeleeAttack2", true);
+                return;
+            case State.ProjectileAttack:
+                draculaAnimator.SetBool("ProjectileAttack", true);
+                return;
         }
+    
     }
+
+    //Choose a random state that isn't the current state
+    private State ChooseRandomState() { 
+        const int numStates = 4;
+
+        bool foundState = false;
+        State calculatedState = State.Idle;
+
+        while (!foundState) {
+            int randomState = Random.Range(0, numStates);
+
+            switch (randomState) {
+                case 0:
+                    calculatedState = State.Idle;
+                    break;
+                case 1:
+                    calculatedState = State.MeleeAttack2;
+                    break;
+                case 2:
+                    calculatedState = State.ProjectileAttack;
+                    break;
+                default:
+                    calculatedState = State.Idle;
+                    break;
+            }
+
+            if (currentState != calculatedState) {
+                foundState = true;
+            }
+        }
+
+        return calculatedState;
+    }
+    
 }
