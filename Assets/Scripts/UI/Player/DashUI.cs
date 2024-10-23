fe847ba108884bc2b3cad049b0c1e96f0bb4d992
Ken Jimeno <kbjimeno@gmail.com>
2024-10-23 13:45:26 -0500

    DashUI = More Robust
    Will automatically reference the player without manually putting it in the inspector

--- Assets/Scripts/UI/Player/DashUI.cs
+++ Assets/Scripts/UI/Player/DashUI.cs
@@ -6,8 +6,19 @@ public class DashUI : MonoBehaviour
     [SerializeField] private GameObject player;
     private bool dashing;
     private Animator animator;
+
     private void Awake(){
         animator = GetComponentInChildren<Animator>();
+
+        if (player == null) {
+            GameObject[] playerParts = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject part in playerParts) {
+                if (part.GetComponent<Moveme>() != null) {
+                    player = part;
+                    break;
+                }
+            }
+        }
     }
 
     void Update(){
