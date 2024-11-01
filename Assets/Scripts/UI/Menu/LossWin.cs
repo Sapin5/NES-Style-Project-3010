254acfb604299f3fe0cfe08b505e82492bc9ebcb
Sapin5 <123406040+Sapin5@users.noreply.github.com>
2024-11-01 11:27:17 -0500

    Fixed some bugs
    NO more hold space to attack, must press to attack :( it was causing issues that im too lazy to fix

--- Assets/Scripts/UI/Menu/LossWin.cs
+++ Assets/Scripts/UI/Menu/LossWin.cs
@@ -1,20 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class LossWin : MonoBehaviour
 {
     [SerializeField] private PlayerHealth health;
     [SerializeField] private GameObject LossScreen;
+    [SerializeField] private string WinScreen;
+
+    [SerializeField] private Health bossHealth;
+
+    private float timer;
     private void Awake(){
         if(health == null){
             health = FindAnyObjectByType<PlayerHealth>();
+        }
+        if(bossHealth == null){
+            bossHealth = FindAnyObjectByType<DraculaStateHandler>().GetComponent<Health>();
         }
     }
 
     private void Update(){
         if(health.RemainingHealth() <= 0){
             LossScreen.SetActive(true);
+        }
+
+        if(bossHealth.GetHealth() <= 0){
+            Timer();
+        }
+    }
+
+    private void Timer(){
+        timer+=Time.deltaTime;
+
+        if(timer > 3f){
+            SceneManager.LoadScene(WinScreen);
         }
     }
 }
